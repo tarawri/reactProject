e8ed6ad4c69bc28e7accd7b32197cb1ce37df2e2
tarawri <tarawri.dev@gmail.com>
2019-10-09 17:03:47 +0300

    Updates

---
+++ precache-manifest.cb512519018bcedd7d4fd02bbfa1f535.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26ce6dce60eef9aee61417451e68e57c",
+    "url": "/hello/index.html"
+  },
+  {
+    "revision": "294b8e90333ae6dc1670",
+    "url": "/hello/static/css/main.25ec012a.chunk.css"
+  },
+  {
+    "revision": "6a98b0cc6e3353ced152",
+    "url": "/hello/static/js/2.89ba8618.chunk.js"
+  },
+  {
+    "revision": "294b8e90333ae6dc1670",
+    "url": "/hello/static/js/main.94beef2d.chunk.js"
+  },
+  {
+    "revision": "a95052e2b86f5bf6ea3c",
+    "url": "/hello/static/js/runtime~main.bbb3531c.js"
+  }
+]);
